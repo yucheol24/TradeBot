2d91426bf3fd522aaf8f123477e10e76839b49a9
Youchul Song <youchul8989@naver.com>
2025-01-03 02:57:08 +0900

    백테스트 기능 추가(sample)

--- backtest.py
+++ backtest.py
@@ -36,5 +36,30 @@ def ma_signal(ma_short_term, ma_long_term):
     else:
         return None
 
+# 백테스트
+def backtest(prices, initial_balance):
+    balance = initial_balance   # 초기 잔고
+    quantity = 0    # 수량
+    ma20 = []
+    ma60 = []
+
+    for i in range(len(prices)):
+        ma20.append(ma(prices[:i+1], 20))
+        ma60.append(ma(prices[:i+1], 60))
+        signal = ma_signal(ma20, ma60)
+
+        if signal == "BUY":
+            amount = balance // prices[i]
+            quantity += amount
+            balance -= amount * prices[i]
+        elif signal == "SELL":
+            amount = quantity   # 전량 매도
+            quantity -= amount
+            balance += amount * prices[i]
+
+        # 현재 수익률
+        roi = ((balance + prices[i] * quantity) / initial_balance - 1) * 100    # %로 표기
+        if signal is not None:
+            print(f"신호: {signal} 수익률: {roi}% 매도 수량: {quantity}주 잔고: {balance}원")
 sample_prices = load_prices("sample.json")
-print(sample_prices)
+backtest(sample_prices, 10000000)
