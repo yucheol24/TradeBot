693bc244b83d485b0f88bf96307709220e485c0e
Youchul Song <youchul8989@naver.com>
2025-01-03 02:45:25 +0900

    함수로 정리

--- backtest.py
+++ backtest.py
@@ -1,25 +1,24 @@
 import json
 
-# MA20, MA60 값 리턴 함수
-def get_stck_prpr(ma_value):
-    ma_list = []
-    for i in range(ma_value, len(stck_prpr_values)):
-        avg = sum(stck_prpr_values[i-ma_value:i]) / ma_value
-        ma_list.append(round(avg, 2))
-    return ma_list
+def load_prices(filename):
+    data = {}
+    result = []
+    with open(filename, "r") as f:
+        data = json.load(f)
+        f.close()
+    for item in data:
+        current_price = int(item["stck_prpr"])
+        result.append(current_price)
 
-data = None
-# sample.json 데이터 읽기
-with open("sample.json", "r") as f:
-    data = json.load(f)
+    return result
 
-# 리스트에 불러온 현재가 저장
-stck_prpr_values = [int(item.get("stck_prpr")) for item in data]
+# 이동 평균선 계산하는 함수
+def ma(values, size):
+    if len(values) >= size:
+        target_values = values[-size:]
+        return sum(target_values) / size
+    else:
+        return None
 
-print(stck_prpr_values)
-
-MA20 = get_stck_prpr(20)
-MA60 = get_stck_prpr(60)
-
-print(MA20)
-print(MA60)
+sample_prices = load_prices("sample.json")
+print(sample_prices)
